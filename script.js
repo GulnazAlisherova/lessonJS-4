8f522a4f9f9b469779bc1ee5926e9e13b449950c
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-09 14:22:33 +0600

    added text

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let example2Button = document.getElementById('example2');
 example2Button.onclick = function() { // при нажатии
-  alert("Nice to meet you");
+  alert("My name is Gulnaz :)");
 }
 
 let example3Button = document.getElementById('example3');
 example3Button.addEventListener('click', function(){ // добавить слушатель события 
   alert("Bye!");
+})
+example3Button.addEventListener('click', function(){ // добавить слушатель события 
+  alert("I just kidding");
 })
