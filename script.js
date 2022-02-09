af13b412dadf20663104a1a115d3af513a305c9a
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-09 14:17:55 +0600

    added click

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ example2Button.onclick = function() { // при нажатии
   alert("Nice to meet you");
 }
 
+let example3Button = document.getElementById('example3');
+example3Button.addEventListener('click', function(){
+  alert("Bye!");
+})
