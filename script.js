da7dc19f4f31568911741e7011b9c00fcf349cac
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-09 14:35:45 +0600

    added length

--- script.js
+++ script.js
@@ -6,18 +6,19 @@ example2Button.onclick = function() { // при нажатии
 let example3Button = document.getElementById('example3');
 example3Button.addEventListener('click', function(){ // добавить слушатель события 
   alert("Bye!");
-})
+});
 example3Button.addEventListener('click', function(){ // добавить слушатель события 
   alert("I just kidding");
-})
+});
 
 
 let example4Button = document.querySelectorById('#example4');
-example4Button.addEventListener('click', function(){ 
+example4Button.addEventListener('click', function() { 
   example4Button.textContent = "Nice to meet you!";
-})
+});
 
-let example5Button = document.getElementById('example5');
-example5Button.addEventListener('click', function(){ 
-  alert("Bye!");
-})
+
+let example5Div = document.querySelectorById('#example5');
+example5Div.addEventListener('click', function() { 
+  alert(example5Div.textContent.length);
+});
