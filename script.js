e075e6d9b0261bd0e3db161269224ce287752c0c
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-09 14:47:55 +0600

    added increaseButton

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
-let example2Button = document.getElementById('example2');
-example2Button.onclick = function() { // при нажатии
-  alert("My name is Gulnaz :)");
-}
+// let example2Button = document.getElementById('example2');
+// example2Button.onclick = function() { // при нажатии
+//   alert("My name is Gulnaz :)");
+// }
 
-let example3Button = document.getElementById('example3');
-example3Button.addEventListener('click', function(){ // добавить слушатель события 
-  alert("Bye!");
-});
-example3Button.addEventListener('click', function(){ // добавить слушатель события 
-  alert("I just kidding");
-});
+// let example3Button = document.getElementById('example3');
+// example3Button.addEventListener('click', function(){ // добавить слушатель события 
+//   alert("Bye!");
+// });
+// example3Button.addEventListener('click', function(){ // добавить слушатель события 
+//   alert("I just kidding");
+// });
 
 
-let example4Button = document.querySelectorById('#example4');
-example4Button.addEventListener('click', function() { 
-  example4Button.textContent = "Nice to meet you!";
-});
+// let example4Button = document.querySelectorById('#example4');
+// example4Button.addEventListener('click', function() { 
+//   example4Button.textContent = "Nice to meet you!";
+// });
 
-let example5Div = document.querySelectorById('#example5');
-example5Div.addEventListener('click', function() { 
-  alert(example5Div.textContent.length);
-});
+// let example5Div = document.querySelectorById('#example5');
+// example5Div.addEventListener('click', function() { 
+//   alert(example5Div.textContent.length);
+// });
 
-let num = 0;
+let num = 5;
 let h1 = document.querySelector('h1');
 let resetButton = document.querySelector('#reset');
 resetButton.addEventListener('click', function(){
-  h1.textContent = "Nice to meet you"
-})
+  //h1.textContent = "Nice to meet you";
+   h1.textContent = num; 
+});
+
+let num = 5;
+let increaseButton = document.querySelector('h1');
+let increaseButton = document.querySelector('#reset');
+resetButton.addEventListener('click', function(){
+  //h1.textContent = "Nice to meet you";
+   h1.textContent = num; 
+});
